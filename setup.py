4ce3c8a681c9d7d41c5e85741aa8e1a2260f8253
Jay Thanki <jayyoges@buffalo.edu>
2023-08-08 01:15:55 -0400

    Added setup.py requirements.txt and created src folder

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(filepath : str) -> List[str]:
+    """
+    This functions reads the names of all the required
+    packages from requirements.txt and returns them as a list.
+    """
+    with open(filepath, 'r') as file:
+        requirements = file.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+        
+    return requirements
+
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Jay Thanki',
+    author_email = 'jayyoges@buffalo.edu',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
